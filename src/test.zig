bd318382282995c1db2fef5a3a264dc7ee6e3281
Ryp <thibault.schueller@protonmail.com>
2021-12-26 21:45:18 +0100

    test: use new workload API

--- src/test.zig
+++ src/test.zig
@@ -2,17 +2,19 @@ const std = @import("std");
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-usingnamespace @import("raytracer.zig");
+const raytracer = @import("raytracer.zig");
 
 const test_seed: u64 = 0xC0FFEE42DEADBEEF;
 
 test "Critical path" {
-    const extent = u32_2{ 128, 128 };
+    const extent = raytracer.u32_2{ 128, 128 };
 
     const allocator: *std.mem.Allocator = std.heap.page_allocator;
 
-    var rt = try create_raytracer_state(allocator, extent, test_seed);
-    defer destroy_raytracer_state(allocator, &rt);
+    var rt = try raytracer.create_raytracer_state(allocator, extent, test_seed);
+    defer raytracer.destroy_raytracer_state(allocator, &rt);
 
-    _ = render_workload(&rt, 4);
+    raytracer.add_fullscreen_workload(&rt);
+
+    _ = raytracer.render_workload(&rt, rt.work_queue_size);
 }
